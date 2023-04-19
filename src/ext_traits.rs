299cd0ae2c9a6ac194961c3e273bbe5cb30dbc6b
Min Qian Lu <59981815+minqianlu@users.noreply.github.com>
2023-04-19 18:52:17 -0400

    wip

---
+++ src/ext_traits.rs
@@ -0,0 +1,13 @@
+use near_sdk::ext_contract;
+
+use crate::*;
+
+#[ext_contract(ext_dao)]
+trait ExtDao{
+
+    fn get_policy(&self) -> Policy;
+
+    fn act_proposal(&mut self, id: u64, action: Action, memo: Option<String>);
+
+    fn add_proposal(&mut self, proposal: ProposalInput);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -6,21 +6,99 @@
  *
  */
 
+mod ext_traits;
+
+use ext_traits::ext_dao;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{log, near_bindgen};
+use near_sdk::{log, near_bindgen, AccountId, Gas, env, Promise, PromiseResult};
+use near_sdk::serde::{Deserialize, Serialize};
+use std::convert::{TryFrom};
+use std::collections::HashSet;
+use near_sdk::json_types::U128;
+
 
 // Define the default message
 const DEFAULT_MESSAGE: &str = "Hello";
+pub const XCC_GAS: Gas = Gas(20_000_000_000_000);
+
+#[derive(Serialize, Deserialize)]
+pub struct ProposalInput {
+    /// Description of this proposal.
+    pub description: String,
+    /// Kind of proposal with relevant information.
+    pub kind: ProposalKind,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum ProposalKind {
+    /// Add member to given role in the policy. This is short cut to updating the whole policy.
+    AddMemberToRole { member_id: AccountId, role: String },
+    /// Remove member to given role in the policy. This is short cut to updating the whole policy.
+    RemoveMemberFromRole { member_id: AccountId, role: String },
+    /// Just a signaling vote, with no execution.
+    Vote,
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+pub struct Policy {
+    /// List of roles and permissions for them in the current policy.
+    pub roles: Vec<RolePermission>,
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize,)]
+pub struct RolePermission {
+    /// Name of the role to display to the user.
+    pub name: String,
+    /// Kind of the role: defines which users this permissions apply.
+    pub kind: RoleKind
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+pub enum RoleKind {
+    /// Matches everyone, who is not matched by other roles.
+    Everyone,
+    /// Member greater or equal than given balance. Can use `1` as non-zero balance.
+    Member(U128),
+    /// Set of accounts.
+    Group(HashSet<AccountId>),
+}
+
+/// Injected Keypom Args struct to be sent to external contracts
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub struct KeypomArgs {
+    pub account_id_field: Option<String>,
+    pub drop_id_field: Option<String>,
+    pub key_id_field: Option<String>,
+    pub funder_id_field: Option<String>
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Debug)]
+pub enum Action {
+    /// Action to add proposal. Used internally.
+    AddProposal,
+    // Action to remove given proposal. Used for immediate deletion in special cases.
+    RemoveProposal,
+    /// Vote to approve given proposal or bounty.
+    VoteApprove,
+    /// Vote to reject given proposal or bounty.
+    VoteReject,
+    /// Vote to remove given proposal or bounty (because it's spam).
+    VoteRemove,
+    /// Finalize proposal, called when it's expired to return the funds
+    /// (or in the future can be used for early proposal closure).
+    Finalize,
+}
 
 // Define the contract structure
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
-pub struct Contract {
+pub struct DaoBot {
     message: String,
 }
 
 // Define the default, which automatically initializes the contract
-impl Default for Contract{
+impl Default for DaoBot{
     fn default() -> Self{
         Self{message: DEFAULT_MESSAGE.to_string()}
     }
@@ -28,17 +106,39 @@ impl Default for Contract{
 
 // Implement the contract structure
 #[near_bindgen]
-impl Contract {
+impl DaoBot {
     // Public method - returns the greeting saved, defaulting to DEFAULT_MESSAGE
-    pub fn get_greeting(&self) -> String {
-        return self.message.clone();
+    pub fn new_proposal(proposal: ProposalInput, keypom_args: KeypomArgs, funder: String, member: String) {
+        
+    }
+
+    pub fn get_roles(dao_contract: String) {
+        ext_dao::ext(AccountId::try_from(dao_contract.to_string()).unwrap())
+        .get_policy()
+        .then(
+            Self::ext(env::current_account_id())
+            .get_roles_callback()
+        );
+    }
+
+    pub fn get_roles_callback(){
+
     }
 
     // Public method - accepts a greeting, such as "howdy", and records it
-    pub fn set_greeting(&mut self, message: String) {
-        // Use env::log to record logs permanently to the blockchain!
-        log!("Saving greeting {}", message);
-        self.message = message;
+    pub fn callback_new_proposal() {
+        assert_eq!(env::promise_results_count(), 1, "ERR_TOO_MANY_RESULTS");
+        match env::promise_result(0) {
+            PromiseResult::NotReady => unreachable!(),
+            PromiseResult::Successful(val) => {
+                if let Ok(is_allowlisted) = near_sdk::serde_json::from_slice::<bool>(&val) {
+                    is_allowlisted
+                } else {
+                    env::panic_str("ERR_WRONG_VAL_RECEIVED")
+                }
+            },
+            PromiseResult::Failed => env::panic_str("ERR_CALL_FAILED"),
+    }
     }
 }
 
@@ -52,21 +152,11 @@ mod tests {
 
     #[test]
     fn get_default_greeting() {
-        let contract = Contract::default();
-        // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(
-            contract.get_greeting(),
-            "Hello".to_string()
-        );
+        
     }
 
     #[test]
     fn set_then_get_greeting() {
-        let mut contract = Contract::default();
-        contract.set_greeting("howdy".to_string());
-        assert_eq!(
-            contract.get_greeting(),
-            "howdy".to_string()
-        );
+        
     }
 }
